fe7a5ca9c5e40218a713a73a35c6f2c4e4eb222c
jamie <384505109@qq.com>
2021-10-08 17:01:45 +0800

    初步完成反射

--- src/main/java/executionEngine/DriverScript.java
+++ src/main/java/executionEngine/DriverScript.java
@@ -3,6 +3,7 @@ package executionEngine;
 import config.ActionKeyWords;
 import utils.ExcelUtils;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -18,11 +19,12 @@ public class DriverScript {
         //可以使用ActionKeyWords keyWord = new ActionKeyWords()
         //初始化时创建一个ActionKeyWords对象
         Class aClass = Class.forName("config.ActionKeyWords");
-        Method[] methods = aClass.getMethods();
+        methods = aClass.getMethods();
     }
 
-    public static void main(String[] args) {
-//        String excelPath = "C:\\Users\\ydbj0140\\IdeaProjects\\SeleniumKeywordFramework\\src\\dataEngine\\dataEngine.xlsx";
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+        //程序进来后到main方法，所以必须先实例化一个DriverScript对象
+        DriverScript script = new DriverScript();
         String excelPath = System.getProperty("user.dir") + "\\src\\main\\java\\dataEngine\\dataEngine.xlsx";
 
         // 加载读取excel文件
@@ -31,23 +33,28 @@ public class DriverScript {
         for (int iRow = 1; iRow <= 6; iRow++) {
             //从excel中读取关键字
             keyWord = ExcelUtils.getCellData(iRow, 3);
-
+            for (int i = 0; i < methods.length; i++) {
+                if (methods[i].getName().equals(keyWord)) {
+                    methods[i].invoke(actionKeyWords);
+                    break;
+                }
+            }
 
             /** 和excel文件中关键字进行对比
-            if (sActionKeyword.equals("openBrowser")) {
-                // 如果Excel文件中存在openBrowser的关键字就会调用openBrowser()方法，进行相关操作；下面其他关键字同理。
-                ActionKeyWords.openBrowser();
-            } else if (sActionKeyword.equals("openUrl")) {
-                ActionKeyWords.openUrl();
-            } else if (sActionKeyword.equals("clickLoginLink")) {
-                ActionKeyWords.clickLoginLink();
-            } else if (sActionKeyword.equals("inputUsername")) {
-                ActionKeyWords.inputUsername();
-            } else if (sActionKeyword.equals("inputPassword")) {
-                ActionKeyWords.inputPassword();
-            } else if (sActionKeyword.equals("clickLoginButton")) {
-                ActionKeyWords.clickLoginButton();
-            }*/
+             if (sActionKeyword.equals("openBrowser")) {
+             // 如果Excel文件中存在openBrowser的关键字就会调用openBrowser()方法，进行相关操作；下面其他关键字同理。
+             ActionKeyWords.openBrowser();
+             } else if (sActionKeyword.equals("openUrl")) {
+             ActionKeyWords.openUrl();
+             } else if (sActionKeyword.equals("clickLoginLink")) {
+             ActionKeyWords.clickLoginLink();
+             } else if (sActionKeyword.equals("inputUsername")) {
+             ActionKeyWords.inputUsername();
+             } else if (sActionKeyword.equals("inputPassword")) {
+             ActionKeyWords.inputPassword();
+             } else if (sActionKeyword.equals("clickLoginButton")) {
+             ActionKeyWords.clickLoginButton();
+             }*/
 
         }
     }
